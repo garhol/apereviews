97cde74e780cae1300a308459c79b4401d7d496b
garhol <chimpoid@gmail.com>
2013-05-24 22:45:47 +0000

    switch off debug, comment out library urls

--- apereview/settings.py
+++ apereview/settings.py
@@ -3,7 +3,7 @@ import os
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -57,7 +57,7 @@ MEDIA_ROOT = '/home/apeman/webapps/apeblog_static/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
