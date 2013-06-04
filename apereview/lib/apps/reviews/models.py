f10172a7582352a6eebcaef5de566afced2f133c
garhol <garhol@3dahoy.co.uk>
2013-06-04 23:53:42 +0100

    reviews with spotify

--- apereview/lib/apps/reviews/models.py
+++ apereview/lib/apps/reviews/models.py
@@ -4,6 +4,15 @@ from apereview.lib.apps.personnel.models import Personnel
 
 from datetime import datetime
 
+from django.db.models.signals import post_save, pre_save
+import requests
+from xml.etree.ElementTree import ElementTree
+from xml.parsers.expat import ExpatError 
+from xml.etree.ElementTree import fromstring
+
+
+spotifyapi_url = "http://ws.spotify.com/search/1/album"
+
 class Review(models.Model):
 
     APE_SCORE = (
@@ -26,6 +35,7 @@ class Review(models.Model):
 
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
+    spotifylink = models.CharField(max_length=255, null=True, blank=True)
     slug = models.CharField(max_length=255)
     label = models.CharField(max_length=255, default="Apeman")
     album_image =  models.ImageField(upload_to='albums/images/%Y/%m/%d', null=True, blank=True)
@@ -48,3 +58,25 @@ class Review(models.Model):
 
     def __unicode__(self):
         return self.album
+
+def pre_rev_save(sender, instance, **kwargs):
+    if not instance.spotifylink:
+        try:
+            data = {'q':instance.album}
+            r = requests.get(spotifyapi_url, params=data)
+            tree = fromstring(r.content.encode('utf-8'))
+            for node in tree.findall('.//{http://www.spotify.com/ns/music/1}album'):
+                for album_name in node.findall('.//{http://www.spotify.com/ns/music/1}name'):
+                    if album_name.text.lower() == instance.album.lower():
+                        album = album_name.text
+                        for artist_name in node.findall('.//{http://www.spotify.com/ns/music/1}artist'):
+                            art = artist_name.find('.//{http://www.spotify.com/ns/music/1}name')
+                            if art.text.lower() == instance.artist.lower():
+                                artist = art.text
+                                href = node.attrib.get('href')
+                                instance.spotifylink = href
+                                break
+        except:
+            pass
+	
+pre_save.connect(pre_rev_save, sender=Review)
