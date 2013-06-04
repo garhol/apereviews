3ad58367d7d4d470b05390490b9deb4bcf0b4408
garhol <garhol@3dahoy.co.uk>
2013-06-04 22:06:06 +0100

    staff pagination

--- apereview/lib/apps/personnel/views.py
+++ apereview/lib/apps/personnel/views.py
@@ -3,6 +3,8 @@ from django.template import RequestContext
 from django.conf import settings
 from itertools import chain
 from operator import attrgetter
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 from .models import Personnel
 from apereview.lib.apps.news.models import News
@@ -16,9 +18,23 @@ def show_personnel(request, staff):
         s = get_object_or_404(Personnel, slug=staff)       
         review_list = Review.objects.filter(review_status='live', reviewer=s).order_by('-date_created')
         news_list = News.objects.filter(news_status='live', reporter=s).order_by('-date_created')
-        context['reviews'] = sorted(
+        
+        r_list = sorted(
             chain(review_list, news_list),
             key=attrgetter('date_created'), reverse=True)
+        paginator = Paginator(r_list, settings.ITEMS_PER_PAGE)
+        page = request.GET.get('page')
+        
+        try:
+            reviews = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            reviews = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            reviews = paginator.page(paginator.num_pages)
+
+        context['reviews'] = reviews       
         context['staff'] = s
     else:
         context['error'] = "Staff matching query does not exist"
