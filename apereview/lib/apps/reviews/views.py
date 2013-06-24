a4ebbb63af1219dc589e8c8264bd780d0f7f7638
garhol <garhol@3dahoy.co.uk>
2013-06-25 00:02:09 +0100

    Add artist listings

--- apereview/lib/apps/reviews/views.py
+++ apereview/lib/apps/reviews/views.py
@@ -28,6 +28,31 @@ def show_review(request, review):
     context['about'] = AboutText.objects.all()[:1].get() 
     return render_to_response(template, context, context_instance=RequestContext(request))
 
+def list_reviews_by_artist(request, artist):
+    context = {}
+    template = 'list_reviews.html'
+    ar = artist.replace('-', ' ')
+    print ar
+    review_list = Review.objects.filter(review_status='live').filter(artist__iexact = ar).order_by('-date_created')
+   
+    paginator = Paginator(review_list, settings.ITEMS_PER_PAGE)
+    page = request.GET.get('page')     
+   
+    try:
+        reviews = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        reviews = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        reviews = paginator.page(paginator.num_pages)
+        
+    context['reviews'] = reviews
+    
+    context['personnel'] = Personnel.objects.all()
+    context['about'] = AboutText.objects.all()[:1].get()
+    return render_to_response(template, context, context_instance=RequestContext(request))
+    
 def list_reviews(request):
     context = {}
     template = 'list_reviews.html'
